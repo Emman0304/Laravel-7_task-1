90cc4943c49a688fe1b73972349268166f42eea7
Emman0304 <emmanuel.official0304@gmail.com>
2022-11-10 17:34:16 +0800

    Update UsersExport.php

--- app/Exports/UsersExport.php
+++ app/Exports/UsersExport.php
@@ -4,14 +4,16 @@ namespace App\Exports;
 
 use App\Product;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithEvents;
 use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Events\AfterSheet;
 
-class UsersExport implements FromCollection,WithHeadings
+class UsersExport implements FromCollection,WithHeadings,WithEvents
 {
     public function headings(): array
     {
         return [
-            'lastname',
+            'Lastname',
             'firstname',
             'mi',
             'age',
@@ -32,6 +34,18 @@ class UsersExport implements FromCollection,WithHeadings
     {
         return collect(Product::getStudents());
         
+    }
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class    => function(AfterSheet $event) {
+   
+                $event->sheet->getDelegate()->getStyle('A1:J1')
+                                ->getFont()
+                                ->setBold(true);
+   
+            },
+        ];
     }
 
 }
